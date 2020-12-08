37f5cb5e2020d89743ba22d72dbb4d0c9285c246
atamoghlan <atamoghlan.a.aliyev@gmail.com>
2020-12-08 13:29:44 +0400

    completed

---
+++ babel.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  presets: ['module:metro-react-native-babel-preset'],
+};
+<View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+<Text>Hello, world!</Text>
+</View>
